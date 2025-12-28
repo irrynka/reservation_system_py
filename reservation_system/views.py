b9d6cdaf10efd45810fb6f4267a6ef5e804384d5
irrynka <irinakorcinska7@gmail.com>
2025-12-28 14:39:54 +0100

    Add login page and fix LOGIN_URL redirect

--- reservation_system/views.py
+++ reservation_system/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .models import Room, Booking
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 def home(request):
@@ -33,3 +35,31 @@ def booking_list(request, room_id):
 
 def booking_success(request):
     return render(request, 'booking_success.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('room_list')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'register.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('room_list')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login.html', {'form': form})
+
+
